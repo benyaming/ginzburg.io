b85232be618a83863286019455d6bd6c23451987
Benyomin Ginzburg <benyomin.94@gmail.com>
2019-03-04 15:51:54 +0200

    Add trumbowig widget to admin

--- ginzburgio/blog/admin.py
+++ ginzburgio/blog/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
+from django.db.models import TextField
 
 from .models import Post, Category, Tag
+from .widgets import ContentWidget
 
 
-admin.site.register(Post, Category, Tag)
+class Admin(admin.ModelAdmin):
+    formfield_overrides = {TextField: {'widget': ContentWidget}}
+
+
+admin.site.register(Post, Admin)
+admin.site.register((Category, Tag))
---
+++ ginzburgio/blog/widgets.py
@@ -0,0 +1,63 @@
+from trumbowyg.widgets import TrumbowygWidget
+from django.utils.safestring import mark_safe
+from django.urls import reverse
+
+from trumbowyg.widgets import get_trumbowyg_language
+
+
+class ContentWidget(TrumbowygWidget):
+    class Media:
+        css = {
+            'all': (
+                'trumbowyg/ui/trumbowyg.css',
+                'trumbowyg/admin.css',
+            )
+        }
+        js = [
+            '//ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js',
+            'trumbowyg/trumbowyg.min.js',
+            'trumbowyg/plugins/upload/trumbowyg.upload.js',
+            f'trumbowyg/langs/{get_trumbowyg_language()}.min.js'
+        ]
+
+    def render(self, name, value, attrs=None, renderer=None):
+        output = super(TrumbowygWidget, self).render(name, value, attrs)
+        script = f'''
+            <script>
+            $("#id_{name}").trumbowyg({{
+                lang: "{get_trumbowyg_language()}",
+                semantic: true,
+                resetCss: true,
+                autogrow: false,
+                removeformatPasted: true,
+                btnsDef: {{
+                    image: {{
+                        dropdown: ['upload', 'insertImage', 'base64', 'noembed'],
+                        ico: 'insertImage'
+                    }}
+                }},
+                btns: [
+                       ['formatting'],
+                       'btnGrp-semantic',
+                       ['link'],
+                       ['image'],
+                       'btnGrp-justify',
+                       'btnGrp-lists',
+                       ['horizontalRule'],
+                       ['removeformat'],
+                       ['fullscreen'],
+                       ['viewHTML']
+                ],
+                plugins: {{
+                    upload: {{
+                        serverPath: '{reverse('trumbowyg_upload_image')}',
+                        fileFieldName: 'image',
+                        statusPropertyName: 'message',
+                        urlPropertyName: 'file'
+                    }}
+                }}
+            }});
+            </script>
+        '''
+        output += mark_safe(script)
+        return output
